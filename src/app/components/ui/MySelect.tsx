1c7d5ffe25fca4e26dca584eae70606eca491b7e
EugeniaRS <kenayanrkt@gmail.com>
2024-05-07 23:42:43 -0600

    cambios recientes

--- src/app/components/ui/MySelect.tsx
+++ src/app/components/ui/MySelect.tsx
@@ -1,41 +1,46 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
+import { useEffect, useState } from "react";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
 
 interface Props {
-	options: { value: string; label: string }[];
-	value: { value: string; label: string };
-	onChange: (value: { value: string; label: string }) => void;
-	placeholder: string;
+  label: string;
+  options: { value: string; label: string }[];
+  value: { value: string; label: string };
+  onChange: (value: { value: string; label: string }) => void;
+  placeholder: string;
 }
 export default function MySelect({
-	options,
-	value,
-	onChange,
-	placeholder,
-	...rest
+  label,
+  options,
+  value,
+  onChange,
+  placeholder,
+  ...rest
 }: Props) {
-	const id = Date.now().toString();
-	const animatedComponents = makeAnimated();
-	const [isMounted, setIsMounted] = useState(false);
+  const id = Date.now().toString();
+  const animatedComponents = makeAnimated();
+  const [isMounted, setIsMounted] = useState(false);
 
-	useEffect(() => setIsMounted(true), []);
+  useEffect(() => setIsMounted(true), []);
 
-	return (
-		isMounted && (
-			<Select
-				{...rest}
-				id={id}
-				options={options}
-				closeMenuOnSelect={true}
-				components={animatedComponents}
-				placeholder={placeholder}
-				className="block p-1 w-full"
-				value={value}
-				onChange={onChange}
-			/>
-		)
-	);
+  return (
+    isMounted && (
+      <div className="flex flex-col items-start gap-2 justify-center w-full">
+        <label>{label}</label>
+        <Select
+          {...rest}
+          id={id}
+          options={options}
+          closeMenuOnSelect={true}
+          components={animatedComponents}
+          placeholder={placeholder}
+          className="block p-1 w-full"
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    )
+  );
 }
